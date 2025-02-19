98f4e9161cc3f35f7d8be21763857b2530f111a8
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-02-19 18:50:16 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  region = "us-west-2" # Change to your preferred region
+}
+
+resource "aws_lambda_function" "sg_outbound_checker" {
+  filename      = "lambda_function.zip" # You need to package your Python function into this zip file
+  function_name = "sg-outbound-checker"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "sg_checker_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "sg_checker_lambda_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:DescribeSecurityGroups",
+          "ec2:RevokeSecurityGroupEgress"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudwatch_event_rule" "every_minute" {
+  name                = "every-minute-trigger"
+  description         = "Fires every minute"
+  schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "check_sg_rule" {
+  rule      = aws_cloudwatch_event_rule.every_minute.name
+  target_id = "lambda-check-sg-rule"
+  arn       = aws_lambda_function.sg_outbound_checker.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sg_outbound_checker.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_minute.arn
+}
